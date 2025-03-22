02d648dcb02e20e6b81277f57c59e50f80165faf
kim <ktnguy32@uni.edu>
2025-03-22 18:43:22 -0500

    updated home stuff

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import { useEffect, useState } from "react";
-import "./Home.module.css";
+import styles from "./Home.module.css";
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 const brand_logo =
   "/images/logo.png";
+  
 
 export default function Home() {
+
   return (
-    <div className= "navBar">
-      <p>Home </p>
-      <img src= {brand_logo} className = "logo"
+    <div className={styles.container} id ="begin">
+      
+      <img src= {brand_logo} className ={styles.logo}
     alt="logo icon"/>
+     <h2 className={styles.mainText}>Let's get this over with</h2>
+  <h3 className={styles.subText}>You clearly need help managing your money.</h3>
+  <a href="/signup">
+  <Button variant="dark" className={styles.button}>
+      Control Yourself
+    </Button>
+    </a>
     </div>
   )
 }
